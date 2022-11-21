57ce80de7980f4e20a20ae81bac3f29fccad44a2
Ignacio Febles <wintotum@gmail.com>
2022-11-21 16:17:59 +0100

    21-11-2022

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_11_16_102245) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
