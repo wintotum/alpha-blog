1d530284880147df4b8c1d2cb19f41ea113185e4
Ignacio Febles Rivas <ifeblesr@eservicios.indra.es>
2022-11-25 17:18:23 +0100

    Add users create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, 
             length: { minimum: 3, maximum: 25 }
@@ -6,5 +7,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 
 end
