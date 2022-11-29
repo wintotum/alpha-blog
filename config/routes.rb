ed316f1f0d0d49e99e18d4be2f51b8cb2bd6d772
Ignacio Febles Rivas <ifeblesr@eservicios.indra.es>
2022-11-29 18:44:42 +0100

    varias validaciones

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
-  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
   get 'signup', to: 'users#new'
-  # post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 end
